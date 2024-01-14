25c01ab9819a5ff4ee7457c4ad667e71e7fa99b3
Janishar Ali <janishar.ali@gmail.com>
2024-01-14 23:01:34 +0530

    add docker configuration

---
+++ .resources/init-mongo.js
@@ -0,0 +1,59 @@
+function seed(dbName, user, password) {
+  db = db.getSiblingDB(dbName);
+  db.createUser({
+    user: user,
+    pwd: password,
+    roles: [{ role: 'readWrite', db: dbName }],
+  });
+
+  db.createCollection('api_keys');
+  db.createCollection('roles');
+
+  db.api_keys.insert({
+    key: 'GCMUDiuY5a7WvyUNt9n3QztToSHzK7Uj',
+    permissions: ['GENERAL'],
+    comments: ['To be used by the xyz vendor'],
+    version: 1,
+    status: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+
+  db.roles.insertMany([
+    {
+      code: 'VIEWER',
+      status: true,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      code: 'MANAGER',
+      status: true,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      code: 'ADMIN',
+      status: true,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ]);
+
+  db.users.insert({
+    name: 'Admin',
+    email: 'admin@xyz.com',
+    profilePicUrl: 'http://localhost:3000/files/image/wimm.png',
+    password: '$2a$10$psWmSrmtyZYvtIt/FuJL1OLqsK3iR1fZz5.wUYFuSNkkt.EOX9mLa', // hash of password: changeit
+    roles: db.roles
+      .find({})
+      .toArray()
+      .map((role) => role._id),
+    status: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+}
+
+seed('wimm-db', 'wimm-db-user', 'changeit');
+seed('wimm-test-db', 'wimm-test-db-user', 'changeit');
